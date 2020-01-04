165d0c746160616c53d69a69c9aab0e42fce2550
tanghehe1@163.com <tanghehe1@163.com>
2020-01-04 14:50:36 +0800

    v1

---
+++ test/pages/info/info.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad:function(e){
+			//console.log(e);
+			var newsid=e.newsid;
+			console.log(newsid);
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+newsid,
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log(res);
+					this.title = res.data.title;
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+			
+		},
+		
+	}
+</script>
+
+<style>
+.content{padding: 10upx 2%; width: 96%; flex-wrap: wrap; } 
+.title{line-height: 2em; font-weight: 800; font-size: 30upx;}
+</style>
